89879d03b0509b075ee508f2d644b291b32a0c97
Abhishek Pandey <abhishekpandey5403@gmail.com>
2023-01-09 11:48:14 +0530

    Drum-Kit

---
+++ script.js
@@ -0,0 +1,64 @@
+let l = document.querySelectorAll(".drum").length;
+for (let i = 0; i < l; i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", function ()
+    {
+        let buttoninnerhtml = this.innerHTML;
+        Makesound(buttoninnerhtml);
+        ButtonAnimation(buttoninnerhtml);
+    });
+}
+
+
+document.addEventListener("keypress", function (event) {
+    Makesound(event.key);
+    ButtonAnimation(event.key);
+});
+
+function Makesound(key)
+{
+    switch (key) {
+      case "w":
+        let audio1 = new Audio("sounds/tom-1.mp3");
+        audio1.play();
+        break;
+      case "a":
+        let audio2 = new Audio("sounds/tom-2.mp3");
+        audio2.play();
+        break;
+      case "s":
+        let audio3 = new Audio("sounds/tom-3.mp3");
+        audio3.play();
+        break;
+      case "d":
+        let audio4 = new Audio("sounds/tom-4.mp3");
+        audio4.play();
+        break;
+      case "j":
+        let audio5 = new Audio("sounds/snare.mp3");
+        audio5.play();
+        break;
+      case "k":
+        let audio6 = new Audio("sounds/kick-bass.mp3");
+        audio6.play();
+        break;
+      case "l":
+        let audio7 = new Audio("sounds/crash.mp3");
+        audio7.play();
+        break;
+      default:
+        console.log("key selected was not correct!!!");
+        break;
+    }
+}
+function ButtonAnimation(currentkey)
+{
+    let activebutton = document.querySelector("." + currentkey);
+    activebutton.classList.add("pressed");
+    setTimeout(function ()
+    {
+        activebutton.classList.remove("pressed");
+
+     },100)
+
+ }
